68eb5489ea84d5d50297b6b7fd8dacc76f6f94e9
defaxed <defaxed@gmail.com>
2018-03-12 00:40:43 +0200

    player box

--- src/Components/RockPaperScissor/RPSControls.js
+++ src/Components/RockPaperScissor/RPSControls.js
@@ -1,31 +1,63 @@
 import React from 'react'
 import rxjsConfig from 'recompose/rxjsObservableConfig'
-import { setObservableConfig, createEventHandler, componentFromStream } from 'recompose'
+import { setObservableConfig, createEventHandler, componentFromStream, withState } from 'recompose'
 import { Observable } from 'rxjs'
 import { switchMap } from 'rxjs/add/operator/switchMap'
 setObservableConfig(rxjsConfig)
 
-const RPSControls = ({ counter, loadingClick, loadingReset, handleClick, handleReset, handleCounter }) => {
-
-    // const handleMyClick = e => {
-    //     console.log(e.target.value)
-    //     pSelect(e.target.value)
-
-    //     //this will start the observable, wired in the stream wrapper component
-    //     handleClick()
-    // }
 
 
-    handleCounter(counter)
 
+const RPSControls = ({ counter, loadingClick, loadingReset, handleClick, handleReset, choiceHandler, choice }) => {
+    
+    let pbbLoading = !loadingClick
+
+    if (counter === 3) {
+        pbbLoading = true
+    }
 
     return (
         <div>
-            <button disabled={loadingClick} onClick={handleClick}>Start</button>
-            <br />
-            <br />
-            <h2>{counter}</h2>
-            <button disabled={loadingReset} onClick={handleReset}>Reset</button>
+            <div style={{
+                marginTop: 40,
+                display: 'flex',
+                justifyContent: 'center',
+                flexDirection: 'column',
+                textAlign: 'center',
+            }}>
+                <div>
+                    <button disabled={loadingClick} onClick={handleClick}>Start</button>
+                    <br />
+                    <br />
+                    <h2>{counter}</h2>
+                    <button disabled={loadingReset} onClick={handleReset}>Reset</button>
+                </div>
+            </div>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                textAlign: 'center',
+                marginTop: 40
+            }}>
+                <div style={{
+                    width: '100%',
+                    border: '1px solid black'
+                }}>
+                    <h1>PlayerBox</h1>
+                    <button disabled={pbbLoading} value="rock" onClick={choiceHandler}>Rock</button>
+                    <button disabled={pbbLoading} value="paper" onClick={choiceHandler}> Paper</button>
+                    <button disabled={pbbLoading} value="scissors" onClick={choiceHandler}>Scissors</button>
+                    <h2>PLayer pick: {choice ? choice : ''}</h2>
+                </div>
+                <div style={{
+                    width: '100%',
+                    border: '1px solid black'
+                }}>
+                    <h1>EnemyBox</h1>
+                </div>
+
+            </div>
         </div>
     )
 }
@@ -33,25 +65,29 @@ const RPSControls = ({ counter, loadingClick, loadingReset, handleClick, handleR
 const RPSControlsStream = componentFromStream(props$ => {
     const { stream: handleClick$, handler: handleClick } = createEventHandler()
     const { stream: handleReset$, handler: handleReset } = createEventHandler()
+    const { stream: choiceHandler$, handler: choiceHandler } = createEventHandler()
     let internalCounter = 0
     let loadingClick = false
     let loadingReset = false
 
-    const start = (props) => Observable.interval(props.speed).startWith(0).map(counter => {
+    const interval = (props) => Observable.interval(props.speed).startWith(0).map(counter => {
         internalCounter += 1
         loadingReset = internalCounter === 3 ? false : true
-        return ({ counter: internalCounter, loadingClick: true, loadingReset, handleClick, handleReset, ...props })
+        return ({ counter: internalCounter, loadingClick: true, loadingReset, handleClick, handleReset, choiceHandler, ...props })
     }).take(3)
     return props$.switchMap((props) => {
         return Observable.merge(
             handleClick$
-                .switchMap(e => start(props))
-                .startWith({ counter: 0, loadingClick, loadingReset, handleClick, handleReset, ...props }),
+                .switchMap(e => interval(props))
+                .startWith({ counter: 0, loadingClick, loadingReset, handleClick, handleReset, choiceHandler, ...props })
+                .takeUntil(choiceHandler$),
             handleReset$
                 .map(e => {
                     internalCounter = 0
-                    return ({ counter: 0, loadingClick: false, loadingReset: false, handleClick, handleReset, ...props })
-                })
+                    return ({ counter: 0, loadingClick: false, loadingReset: false, handleClick, handleReset, choiceHandler, ...props })
+                }),
+            choiceHandler$
+                .map(e => ({ ...props, counter: 0, loadingClick: true, loadingReset: true, choice: e.target.value, choiceHandler }))
         )
     }).map(RPSControls)
 })
--- src/Components/RockPaperScissor/RPSEntry.js
+++ src/Components/RockPaperScissor/RPSEntry.js
@@ -3,47 +3,19 @@ import RPSControlsStream from './RPSControls'
 import PlayerBox from './PlayerBox'
 import EnemyBox from './EnemyBox'
 
-const RPSEntry = props => {
+class RPSEntry extends Component {
 
-    let pbbLoading = true
-
-    const handleCounter = (counter) => {
-        console.log(counter)
-        if (counter === 3) {
-            pbbLoading = true
-        } else {
-            pbbLoading = false
-        }
+    constructor(props) {
+        super(props)
     }
 
+    render() {
 
-    return (
-        [
-            <div key="rpscontrols" style={{
-                marginTop: 40,
-                display: 'flex',
-                justifyContent: 'center',
-                flexDirection: 'column',
-                textAlign: 'center',
-            }}>
 
-                <RPSControlsStream speed={500} handleCounter={handleCounter} />
-
-            </div >,
-            <div key="game" style={{
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'center',
-                textAlign: 'center',
-                marginTop: 40
-            }}>
-
-                <PlayerBox pbbLoading={pbbLoading} />
-                <EnemyBox />
-
-            </div>
-        ]
-    )
+        return (
+            <RPSControlsStream speed={5000} />
+        )
+    }
 }
 
 export default RPSEntry
